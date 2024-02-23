6ff5cd5ad8af6f938070c4b1832518822d42cffb
masihgh <masihprogrammer@gmail.com>
2024-02-23 17:46:37 +0330

    simple loggin in firebase :fire:

--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 import './style.css'
 import { app } from './firebase'
-import { getAuth, signInAnonymously } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signInAnonymously } from "firebase/auth";
 
 
 const auth = getAuth(app);
-signInAnonymously(auth)
+auth.signInAnonymously()
     .then(() => {
         console.log('success');
     })
     .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.log(error);
+        console.log(errorCode,errorMessage);
     });
+
+
+    auth.onAuthStateChanged((user) => {
+        console.log(user)
+        if (user) {
+            // You're logged in
+        }
+        else{
+            //You're logged out
+        }
+    })
