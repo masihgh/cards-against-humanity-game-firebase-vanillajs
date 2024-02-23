01786201438a6cd3432e7e9b3863a7d1f2ad4463
masihgh <masihprogrammer@gmail.com>
2024-02-23 17:28:20 +0330

    fixing load config

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+import {firebaseConfig} from '../config'
 
-
-console.log(import.meta.env.FIREBASE_APP_ID)
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
