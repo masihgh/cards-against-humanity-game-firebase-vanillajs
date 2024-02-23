4e2073d74dab290efc8c821ba6cfda911b6f9df5
masihgh <masihprogrammer@gmail.com>
2024-02-23 17:26:24 +0330

    fixing config files

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import './style.css'
+import { app } from './firebase'
+import { getAuth, signInAnonymously } from "firebase/auth";
+
+
+const auth = getAuth(app);
+signInAnonymously(auth)
+    .then(() => {
+        console.log('success');
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error);
+    });
